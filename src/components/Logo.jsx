ea21b6ba8f084aa43f44ff036db8217ec18480c5
ashish <ashishprasad949@gmail.com>
2024-04-03 18:11:26 +0530

    check icon size is correct in vercel

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function Logo({ width = '100px' }) {
+function Logo() {
   return (
     <div>
-      <img className="size-10" src="https://github.githubassets.com/assets/pair-extraordinaire-default-579438a20e01.png" alt="image1" />
+      <img
+        className="w-10"
+        src="https://github.githubassets.com/assets/pair-extraordinaire-default-579438a20e01.png"
+        alt="image1"
+      />
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
