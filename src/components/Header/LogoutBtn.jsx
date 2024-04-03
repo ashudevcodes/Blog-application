a561e022bd2dd6ce96ebf3fdd7da1c6d494b677d
ashish <ashishprasad949@gmail.com>
2024-04-03 17:56:15 +0530

    fix nvbar error

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -5,11 +5,10 @@ import { logout } from "../../store/authSlice";
 
 function LogoutBtn() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const logoutHandler = () => {
     authService.logout().then(() => {
       dispatch(logout());
-      ne;
     });
   };
   return (
