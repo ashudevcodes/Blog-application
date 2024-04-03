923eb4c533145d6f61c29f21e65a7d4d13ab83cc
ashish <ashishprasad949@gmail.com>
2024-04-03 15:33:54 +0530

    re-initialize the whole project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({ width = '100px' }) {
+  return (
+    <div>
+      <img className="size-10" src="https://github.githubassets.com/assets/pair-extraordinaire-default-579438a20e01.png" alt="image1" />
+    </div>
+  )
+}
+
+export default Logo
