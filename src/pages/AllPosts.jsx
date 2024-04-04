f312108e52f122aadb9d0548aa85a4be9c1fb2c8
ashish <ashishprasad949@gmail.com>
2024-04-04 19:55:00 +0530

    change the query of how  the data will show in my post

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,28 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Container, PostCard } from '../components'
 import appwriteService from "../appwrite/config";
+import authService from "../appwrite/auth";
 
 function AllPosts() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {}, [])
-    appwriteService.getPosts([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
-        }
-    })
-  return (
-    <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-                {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard {...post} />
-                    </div>
-                ))}
-            </div>
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            authService.getCurrentUser().then((userDate) => {
+                const filteredDocuments = posts.documents.filter(documents => documents.userId == userDate.$id);
+                console.log(posts);
+                if (filteredDocuments) {
+                    setPosts(filteredDocuments)
+                }
+            });
+
+        });
+    }, [])
+
+    return (
+        <div className='w-full py-10'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/5'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
             </Container>
-    </div>
-  )
+        </div>
+    );
 }
 
 export default AllPosts
