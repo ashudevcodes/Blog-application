c543b4a2f5195f557a0baad003b78f38409daba6
ashish <ashishprasad949@gmail.com>
2024-04-06 17:20:14 +0530

    fix the error now user  will not show all of the another user posts in mypost section

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -2,19 +2,24 @@ import { useState, useEffect } from 'react'
 import { Container, PostCard } from '../components'
 import appwriteService from "../appwrite/config";
 import authService from "../appwrite/auth";
+import { Query } from 'appwrite';
 
 function AllPosts() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {
-        appwriteService.getPosts([]).then((posts) => {
-            authService.getCurrentUser().then((userDate) => {
-                const filteredDocuments = posts.documents.filter(documents => documents.userId == userDate.$id);
-                console.log(posts);
-                if (filteredDocuments) {
-                    setPosts(filteredDocuments)
-                }
-            });
 
+    useEffect(() => {
+        authService.getCurrentUser().then((user) => {
+            // console.log(user)
+            appwriteService.getPosts([Query.equal("userId", String(user.$id))]).then((posts) => {
+                authService.getCurrentUser().then((userDate) => {
+                    const filteredDocuments = posts.documents.filter(documents => documents.userId == userDate.$id);
+                    // console.log(posts);
+                    if (filteredDocuments) {
+                        setPosts(filteredDocuments)
+                    }
+                });
+
+            });
         });
     }, [])
 
@@ -23,7 +28,7 @@ function AllPosts() {
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
+                        <div key={post.$id} className='p-2 w-1/2'>
                             <PostCard {...post} />
                         </div>
                     ))}
